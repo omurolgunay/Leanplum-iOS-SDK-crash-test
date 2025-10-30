d52f669a16e192ce87a14fe7846fde856278c2cb
Ömür <omur.olgunay@gmail.com>
2025-10-30 11:45:11 +0300

    Update Logging.swift

--- LeanplumSDK/LeanplumSDK/ClassesSwift/Utilities/Logging.swift
+++ LeanplumSDK/LeanplumSDK/ClassesSwift/Utilities/Logging.swift
@@ -10,31 +10,17 @@
  */
 enum Log {
     static func info(_ msg: String) {
-        LPLogv(.info, msg, getVaList([]))
     }
     
     static func debug(_ msg: String) {
-        LPLogv(.debug, msg, getVaList([]))
     }
     
     static func error(_ msg: String) {
-        LPLogv(.error, msg, getVaList([]))
     }
     
     static func notificationInfo(_ msg: String) {
-        let level = LPLogManager.logLevel()
-        guard level.rawValue >= LeanplumLogLevel.info.rawValue else {
-            return
-        }
-        NSLog("[LEANPLUM] [INFO]: %@", msg)
-        
     }
     
     static func notificationDebug(_ msg: String) {
-        let level = LPLogManager.logLevel()
-        guard level.rawValue >= LeanplumLogLevel.debug.rawValue else {
-            return
-        }
-        NSLog("[LEANPLUM] [DEBUG]: %@", msg)
     }
 }
